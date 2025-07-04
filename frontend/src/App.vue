a6f0596e074eb29f68a269fdb41da694a80e9899
ahmed-maamoun <57485384+ahmed-maamoun@users.noreply.github.com>
2025-07-04 23:28:19 +0300

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -257,7 +257,7 @@ export default {
     },
     matchHasStarted() {
       if (!this.selectedMatch) return false;
-      const now = new Date(Date.now() * 3600000);
+      const now = new Date();
       const matchTime = new Date(this.selectedMatch.startTime);
       return now >= matchTime;
     }
